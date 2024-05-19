0d09f5bb0ee5214fb552409957a60bd76105c8d1
Darragh Brennan <darragh.brennan88@gmail.com>
2024-05-19 13:26:51 +0100

    new

---
+++ Analysis.py
@@ -0,0 +1,9 @@
+#pands project
+#Programme - Analysis of Iris data set. 
+#Author Darragh Brennan
+
+import pandas as pd # 
+import matplotlib.pyplot as plt # 
+
+
+# Load the Iris dataset import csv
