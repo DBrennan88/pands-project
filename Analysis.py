237a5e1be18f6ab18f34b230c469bc15a82b269b
Darragh Brennan <darragh.brennan88@gmail.com>
2024-05-19 21:28:05 +0100

    Species comparison by group complete

--- Analysis.py
+++ Analysis.py
@@ -47,7 +47,7 @@ plt.savefig("petal_width_histogram.png")
 """""
 # 3. Outputs a scatter plot of each pair of variables. 
 
-colors = {"iris-setosa": "purple", "Iris-versicolor": "yellow", "Iris-virginica": "red"} # my dictionary creating paired values
+colors = {"setosa": "purple", "versicolor": "yellow", "virginica": "red"} # my dictionary creating paired values # labelling difference here caused me errors. 
 
  # define variable as a list to include relevant data a from csv as a list
 sepal_length = (iris_data["sepal_length"])
@@ -96,6 +96,7 @@ plt.title("Sepal Length vs Petal Length")
 plt.savefig("Sepal Length vs Petal Length scatter.png")
 
 # Sepal Width vs Petal Width
+
 plt.figure(figsize=(10, 8))
 plt.scatter(sepal_width, petal_width, c ="red", 
             linewidths = 2, # size of edge of marker
@@ -114,6 +115,7 @@ plt.show()
 # Add code that details species type to compare each variable across the 3 classes of Iris -  need to group by species 
 
 
+# Sepal Length vs Sepal Width
 
 plt.figure(figsize=(8, 6))
 for species_name, color in colors.items(): # boolean runs to create new variables (species_name, color) by pullinh the the 2 "items" in my color dictionary
@@ -129,6 +131,51 @@ plt.title("Sepal Length vs Sepal Width")
 plt.legend(colors)
 plt.show()
 
+# Petal Length vs Petal Width
+
+plt.figure(figsize=(8, 6))
+for species_name, color in colors.items(): # boolean runs to create new variables (species_name, color) by pullinh the the 2 "items" in my color dictionary
+    species_data = iris_data[iris_data["species"] == species_name] 
+    plt.scatter(species_data["petal_length"], species_data["petal_width"], label=species_name, c=color)  
+    linewidths = 2, # size of edge of marker
+    marker ="s", #square marker
+    s = 25 # size of indicator
+
+plt.xlabel("Petal Length")
+plt.ylabel("Petal Width")
+plt.title("Petal Length vs Petal Width") 
+plt.legend(colors)
+plt.show()
 
 
+# Sepal Length vs Petal Length
 
+plt.figure(figsize=(8, 6))
+for species_name, color in colors.items(): # boolean runs to create new variables (species_name, color) by pullinh the the 2 "items" in my color dictionary
+    species_data = iris_data[iris_data["species"] == species_name] 
+    plt.scatter(species_data["sepal_length"], species_data["petal_length"], label=species_name, c=color)  
+    linewidths = 2, # size of edge of marker
+    marker ="s", #square marker
+    s = 25 # size of indicator
+
+plt.xlabel("Sepal Length")
+plt.ylabel("Petal Length")
+plt.title("Sepal Length vs Petal Length") 
+plt.legend(colors)
+plt.show()
+
+# Sepal Width vs Petal Width
+
+plt.figure(figsize=(8, 6))
+for species_name, color in colors.items(): # boolean runs to create new variables (species_name, color) by pullinh the the 2 "items" in my color dictionary
+    species_data = iris_data[iris_data["species"] == species_name] 
+    plt.scatter(species_data["sepal_width"], species_data["petal_width"], label=species_name, c=color)  
+    linewidths = 2, # size of edge of marker
+    marker ="s", #square marker
+    s = 25 # size of indicator
+
+plt.xlabel("Sepal Width")
+plt.ylabel("Petal Width")
+plt.title("Sepal Width vs Petal Width") 
+plt.legend(colors)
+plt.show()
